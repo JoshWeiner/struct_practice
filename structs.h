ca0108cf55cb98fd1ce456d6015c9c8fbc1300b2
Josh Weiner <joshuaweinernyc@gmail.com>
2018-10-02 19:58:05 -0400

    making final additions to .h file

---
+++ structs.h
@@ -0,0 +1,3 @@
+struct australia random_struct(struct australia s);
+void struct_printer(struct australia s);
+struct australia modifier(struct australia s, char * animal_replace, int deadliness_replace);
